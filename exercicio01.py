31175ab29e2c1d680885064bb65424b180b1f4ec
Nádia Souza <nadialuisa@gmail.com>
2024-02-27 21:12:20 -0300

    Adicionando exercicio 01

---
+++ exercicio01.py
@@ -0,0 +1,5 @@
+# Exercício 01: Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+print(len(input ("Digite seu nome: ")))
+
+# Exercício 02: Criar um programa onde o usuário digite dois valores e apareça a soma
+print(input ("Digite o primeiro número: ") + ("Digite segundo número: "))# Aula01_Bootcamp
